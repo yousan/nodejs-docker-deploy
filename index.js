f65b0d3f67b83b5eeb24d72844964cf5c4533fb9
Yousan_O <yousan@l2tp.org>
2017-05-24 16:59:20 +0900

    add production yaml

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const express = require('express');
-const app = express()
-
-app.get('/', function (req, res) {
-    res.send('Hello World!!')
-})
-
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+console.log("hello world2");
